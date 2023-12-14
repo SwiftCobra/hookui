e47a4542fb0a14f18941b17492111238c4f2efa8
Swift Cobra <swiftcobratv@gmail.com>
2023-12-14 14:35:17 -0500

    Adding Tests project

---
+++ HookUIMod.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using HookUILib.Core;
+using Xunit;
+
+namespace HookUIMod.Tests
+{
+    public class PanelSizeTests_IsPrimeShould
+    {
+        [Fact]
+        public void TestSerialization()
+        {
+            var something = new UIExtension();
+            Xunit.Assert.NotNull(something);
+
+        }
+    }
+}
